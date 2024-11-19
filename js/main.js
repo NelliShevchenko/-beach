becfc88349d4b15d3dc88b7b076565a06828bf7a
NelliShevchenko <gho020507@gmail.com>
2024-11-19 16:11:12 +0200

    task

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        dots: true
+    });  
+});
